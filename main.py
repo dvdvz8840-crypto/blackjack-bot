a3a76c75bf9c9b86f7cb65748deb5e868ec33e89
dvdvz8840-crypto <dvdvz8840@gmail.com>
2026-02-14 21:09:56 +0300

    Update main.py

--- main.py
+++ main.py
@@ -237,7 +237,6 @@ def blackjack(message):
                      reply_markup=game_keyboard(can_double=True))
 
 # ---------------- Игровые кнопки ----------------
-# ---------------- Игровые кнопки ----------------
 @bot.callback_query_handler(func=lambda c: c.data in ["hit", "stand", "cash", "double"])
 def game_actions(callback):
     user_id = callback.from_user.id
